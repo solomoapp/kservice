86fc95de0e7dbaed18774a103f1bef63b7446515
yuming <yuming83@qq.com>
2017-10-24 10:54:31 +0800

    数据库查询的异常处理

--- app/utils/mysqlUtil.js
+++ app/utils/mysqlUtil.js
@@ -1,6 +1,7 @@
 //包含了数据库连接池控制，连接建立、释放管理，执行Dao发起的数据库操作请求
 const mysql = require('mysql');
 const config = require('./../../config/config.local.js');
+const ApiError = require('../error/ApiError.js');
 
 var connectionPool = mysql.createPool({
     'host' : config.database.host,
@@ -29,41 +30,46 @@ var execQuery = sqlOptions => {
         connectionPool.getConnection((error, connection) => {
             if(error) {
                 console.log('Get connection from mysql pool failed !');
-                throw error;
+                //throw error;
+                reject(new ApiError('_9527', -1, 'Get connection from mysql pool failed !'));
             }
 
             var sql = sqlOptions['sql'];
             var args = sqlOptions['args'];
-			
-			if(!args) {
-				var query = connection.query(sql, (error, results) => {
-					if(error) {
-						console.log('Execute query error !');
-						throw error;
-					}
+            
+            if(!args) {
+                var query = connection.query(sql, (error, results) => {
+                    if(error) {
+                        console.log('Execute query error !');
+                        //throw error;
+                        reject(new ApiError('_9527', -1, 'Execute query error !'));
+                    }
 
-					resolve(results);
-				});
-			} else {
-				var query = connection.query(sql, args, function(error, results) {
-					if(error) {
-						console.log('Execute query error !');
-						throw error;
-					}
+                    resolve(results);
+                });
+            } else {
+                var query = connection.query(sql, args, function(error, results) {
+                    if(error) {
+                        console.log('Execute query error !');
+                        //throw error;
+                        reject(new ApiError('_9527', -1, 'Execute query error !'));
+                    }
 
-					resolve(results);
-				});
-			}
+                    resolve(results);
+                });
+            }
 
             connection.release(function(error) {
                 if(error) {
                     console.log('Mysql connection close failed !');
-                    throw error;
+                    //throw error;
                 }
             });
         });
     }).then(function (chunk) {
         return chunk;
+    }).catch(function(error) {
+        throw error;
     });
 
     return results;
@@ -72,4 +78,4 @@ var execQuery = sqlOptions => {
 module.exports = {
     release : release,
     execQuery : execQuery
-}
+};
